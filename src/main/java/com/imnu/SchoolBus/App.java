df70ce239521eb8578edc00702ee6dba8c335556
20161104571 <852169778@qq.com>
2019-12-18 15:31:49 +0800

    1111

--- src/main/java/com/imnu/SchoolBus/App.java
+++ src/main/java/com/imnu/SchoolBus/App.java
@@ -1,5 +1,7 @@
 package com.imnu.SchoolBus;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,5 +18,6 @@ public class App
 {
 	 public static void main( String[] args ) {
 	    	SpringApplication.run(App.class, args);
+	    	print();
 	    }
 }
